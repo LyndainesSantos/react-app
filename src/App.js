ac1d4d443f31ef654463f4f193d49764170b777f
LyndainesSantos <lyndainesaraujo@gmail.com>
2023-05-12 23:14:03 -0300

    Change description

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          DevOps Básico IT Talent - Branch: MAIN
+          DevOps Básico IT Talent - Mudanças aula 26-04-2023
         </p>
         <a
           className="App-link"
