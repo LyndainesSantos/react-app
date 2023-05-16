e96405373ec9880b86fddd29ecdb4b0ccb708194
LyndainesSantos <125848451+LyndainesSantos@users.noreply.github.com>
2023-05-16 13:21:24 -0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          DevOps Básico IT Talent - Release
+          DevOps Básico IT Talent
         </p>
         <a
           className="App-link"
